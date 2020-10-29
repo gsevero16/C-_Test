88a80ceb8f04635a9b24abcc92404259687f11f6
Gabriel Severo <gsevero16@gmail.com>
2020-10-29 10:05:50 -0300

    Template inicial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C__Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
